38cef7c6cc13709f7fcd8cb1f2135719faf67bf9
unknown <hmmasud635@gmail.com>
2025-05-15 16:04:32 +0600

    new added

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,15 +13,15 @@ const Login = () => {
           <div className='relative w-full h-12.5 border-b-2 border-[#000b13] my-10'>
             <MdEmail className='absolute right-2 text-2xl top-4' />
             <input className='absolute w-full text-white bg-transparent h-full text-xl outline-none peer' type="email" id='email' required />
-            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-25px] peer-valid:translate-y-[-25px]' For="email">Email</label>
+            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-25px] peer-valid:translate-y-[-25px]' htmlFor="email">Email</label>
           </div>
           <div className='relative w-full h-12.5 border-b-2 border-[#000b13] my-8 mb-2'>
             <FaRegEyeSlash className='absolute right-2 text-2xl top-4' />
             <input className='absolute w-full text-white bg-transparent h-full text-xl outline-none peer' type="password" id='password' required />
-            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-25px] peer-valid:translate-y-[-25px]' For="password">Password</label>
+            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-25px] peer-valid:translate-y-[-25px]' htmlFor="password">Password</label>
           </div>
           <div className='flex justify-between mt-5'>
-            <label className='text-xl' for=""><input className='mr-1' type="checkbox" />Remember me</label>
+            <label className='text-xl' htmlFor=""><input className='mr-1' type="checkbox" />Remember me</label>
             <a className='hover:underline text-xl' href="">Forgot password</a>
           </div>
           <button type="submit" className='w-full py-5 my-8 bg-black rounded-md text-3xl text-white font-semibold'>Login</button>
--- src/Pages/Sign_up.jsx
+++ src/Pages/Sign_up.jsx
@@ -1,73 +1,126 @@
-import React, { useState } from 'react'
-import { FaRegEyeSlash, FaUserTie } from 'react-icons/fa'
-import { MdEmail } from 'react-icons/md'
-import { Link } from 'react-router'
-import Colloquy from '../Component/Colloquy'
+import React, { useState } from "react";
+import { FaRegEyeSlash, FaUserTie } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { Link } from "react-router";
+import Colloquy from "../Component/Colloquy";
+import toast, { Toaster } from "react-hot-toast";
 
 const Sign_up = () => {
   const [userInfo, setUserInfo] = useState({
     name: "",
     email: "",
     password: "",
-  })
+  });
 
   const handleUser = (e) => {
     setUserInfo((prev) => {
-      return { ...prev, name: e.target.value }
-    })
-  }
+      return { ...prev, name: e.target.value };
+    });
+  };
 
   let handleEmail = (e) => {
     setUserInfo((prev) => {
-      return { ...prev, email: e.target.value }
-    })
-  }
+      return { ...prev, email: e.target.value };
+    });
+  };
 
   let handlePassword = (e) => {
     setUserInfo((prev) => {
-      return { ...prev, password: e.target.value }
-    })
-  }
+      return { ...prev, password: e.target.value };
+    });
+  };
 
   const handleSignup = (e) => {
     e.preventDefault();
-    if (!userInfo.name || !userInfo.email || !userInfo.password ) {
-      alert("all file add dd")
+    if (!userInfo.name || !userInfo.email || !userInfo.password) {
+      toast.error("all fileds are required");
+    
+    }else {
+        toast.success("Successfully Sign Up");
     }
-  }
+  };
 
   return (
-    <div className='flex'>
-      <div className='w-130 bg-sky-800/50 p-10 border-2 border-[#ffffff46] rounded-2xl backdrop-blur-md relative m-50 ml-80'>
-        <h1 className='text-4xl font-bold text-center'>Sign up</h1>
+    <div className="flex">
+      <Toaster position="top-right" reverseOrder={false} />
+      <div className="w-130 bg-sky-800/50 p-10 border-2 border-[#ffffff46] rounded-2xl backdrop-blur-md relative m-50 ml-80">
+        <h1 className="text-4xl font-bold text-center">Sign up</h1>
         <form onSubmit={handleSignup}>
-          <div className='relative w-full h-12.5 border-b-2 border-[#000b13] my-10'>
-            <FaUserTie className='absolute right-2 text-2xl top-4' />
-            <input onChange={handleUser}  className='absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer' type="text" id='text' name='text' />
-            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]' htmlFor='text'>User</label>
+          <div className="relative w-full h-12.5 border-b-2 border-[#000b13] my-10">
+            <FaUserTie className="absolute right-2 text-2xl top-4" />
+            <input
+              onChange={handleUser}
+              className="absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer"
+              type="text"
+              id="text"
+              name="text"
+            />
+            <label
+              className="text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]"
+              htmlFor="text"
+            >
+              User
+            </label>
           </div>
-          <div className='relative w-full h-12.5 border-b-2 border-[#000b13] my-8'>
-            <MdEmail className='absolute right-2 text-2xl top-4' />
-            <input onChange={handleEmail} className='absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer' type="email" id='email'  />
-            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]' htmlFor="email">Email</label>
+          <div className="relative w-full h-12.5 border-b-2 border-[#000b13] my-8">
+            <MdEmail className="absolute right-2 text-2xl top-4" />
+            <input
+              onChange={handleEmail}
+              className="absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer"
+              type="email"
+              id="email"
+            />
+            <label
+              className="text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]"
+              htmlFor="email"
+            >
+              Email
+            </label>
           </div>
-          <div className='relative w-full h-12.5 border-b-2 border-[#000b13] mt-8.5 mb-2'>
-            <FaRegEyeSlash className='absolute right-2 text-2xl top-4' />
-            <input onChange={handlePassword} className='absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer' type="password" id='password'  />
-            <label className='text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]' htmlFor="password">Create Password</label>
+          <div className="relative w-full h-12.5 border-b-2 border-[#000b13] mt-8.5 mb-2">
+            <FaRegEyeSlash className="absolute right-2 text-2xl top-4" />
+            <input
+              onChange={handlePassword}
+              className="absolute w-full text-white/80 bg-transparent h-full text-2xl outline-none peer"
+              type="password"
+              id="password"
+            />
+            <label
+              className="text-xl text-black font-medium duration-400 top-3 absolute peer-focus:translate-y-[-35px] peer-valid:translate-y-[-35px]"
+              htmlFor="password"
+            >
+              Create Password
+            </label>
           </div>
-          <div className='flex justify-between mt-5'>
-            <label className='text-xl' htmlFor=""><input className='mr-1' type="checkbox" />I agree to the terms & conditions</label>
+          <div className="flex justify-between mt-5">
+            <label className="text-xl" htmlFor="">
+              <input className="mr-1" type="checkbox" />I agree to the terms &
+              conditions
+            </label>
           </div>
-          <button type="submit" className='w-full py-5 my-8 bg-black rounded-md text-3xl text-white font-semibold'>Sign up</button>
+          <button
+            type="submit"
+            className="w-full py-5 my-8 bg-black rounded-md text-3xl text-white font-semibold cursor-pointer"
+          >
+            Sign up
+          </button>
           <div>
-            <p className='text-center text-xl'>Already have an account <Link to={"/login"} className='font-semibold hover:underline ml-2' href="">Login</Link></p>
+            <p className="text-center text-xl">
+              Already have an account{" "}
+              <Link
+                to={"/login"}
+                className="font-semibold hover:underline ml-2"
+                href=""
+              >
+                Login
+              </Link>
+            </p>
           </div>
         </form>
       </div>
       <Colloquy />
     </div>
-  )
-}
+  );
+};
 
-export default Sign_up
+export default Sign_up;
