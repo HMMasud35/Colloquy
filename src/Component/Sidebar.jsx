c4b2fa360abca479ca3c97062c32afb0df8cc950
unknown <hmmasud635@gmail.com>
2025-05-28 14:39:29 +0600

    new add

--- src/Component/Sidebar.jsx
+++ src/Component/Sidebar.jsx
@@ -8,7 +8,7 @@ import { Link } from 'react-router'
 const Sidebar = () => {
   return (
     <div>
-      <div className='bg-sky-800/80 h-screen shadow-md shadow-gray-100'>
+      <div className='bg-sky-800/80 h-screen border-r-3 border-gray-700/80'>
         <div className='border-b-5 border-gray-300 py-3'>
           <img className='m-auto w-30' src="/image/logo.png" alt="logo.png" />
           <h1 className='text-center text-4xl font-bold text-white'>Colloquy</h1>
@@ -20,9 +20,9 @@ const Sidebar = () => {
           <li className='p-5 pr-12 hover:bg-gray-800/30 w-full h-full'><a className='text-2xl text-white flex' href=""><GrChannel className='w-10 mr-5 text-4xl text-gray-300' />Chanel</a></li>
           <li className='p-5 pr-12 hover:bg-gray-800/30 w-full h-full'><a className='text-2xl text-white flex' href=""><RiUserCommunityFill className='w-10 mr-5 text-4xl text-gray-300' />Status</a></li>
         </ul>
-        <ul className='mt-40'>
+        <ul className='mt-50'>
           <li className='mb-30 py-5 px-5 hover:bg-gray-800/30 w-full h-full'><Link to={"/login"} className='text-2xl text-white flex ' href=""><IoSettingsOutline className='w-10 mr-5 text-4xl text-gray-300' />Setting</Link></li>
-          <li className='bg-sky-700 mt-[-110px] p-3 hover:bg-gray-800/30 border-2 border-sky-800 rounded-4xl flex items-center text-xl font-semibold text-white '><img className='w-20 h-20 bg-amber-200 rounded-full mr-3 ' src="" alt="" />Hasan Masud</li>
+          <li className='bg-sky-700 mt-[-110px] p-1 hover:bg-gray-800/30 border-2 border-sky-800 rounded-4xl flex items-center text-xl font-semibold text-white '><img className='w-18 h-18 bg-amber-200 rounded-full mr-3 ' src="" alt="" />Hasan Masud</li>
         </ul>
       </div>
     </div>
