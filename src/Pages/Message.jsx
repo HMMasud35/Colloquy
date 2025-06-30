e825c2f0bbdb6a507049f33d00784a1dab65f549
Hasan Masud <hmmasud635@gmail.com>
2025-06-30 10:12:09 +0600

    new add file

--- src/Pages/Message.jsx
+++ src/Pages/Message.jsx
@@ -10,6 +10,7 @@ import MessagesList from '../Component/MessageList'
 import { useSelector } from 'react-redux'
 import { getDatabase, onValue, push, ref, set } from 'firebase/database'
 import { auth } from '../firebase.config'
+import moment from 'moment/moment'
 
 const Message = () => {
   let [sms, setSms] = useState(null)
@@ -48,14 +49,12 @@ const Message = () => {
       reciverid: user.id,
       reciverphoto: user.photo,
       sms: sms,
-      // date : 
+      date: `${new Date().getFullYear()}/${new Date().getMonth() + 1}/${new Date().getDate()}:
+        ${new Date().getHours()}:${new Date().getMinutes()}`,
     }).then(() => {
       setSms("")
-      // remove(ref(db, "friendrequest/", item.id))
     })
   }
-
-  console.log(smslist);
 
   return (
     <div>
@@ -63,12 +62,12 @@ const Message = () => {
         <MessagesList />
         {user?.id ? (
           <div className='h-screen relative'>
-            <div className='bg-sky-800/95 fixed w-6xl z-10 h-25 flex justify-between items-center p-8'>
+            <div className='bg-sky-800/95 w-6xl z-10 h-25 flex justify-between items-center p-8'>
               <div className='flex gap-5'>
                 <img className='w-17 h-17 my-1 rounded-full border-3 border-white/50 shadow-sm shadow-black/70' src={user?.photo} alt="User Photo" />
                 <div>
                   <h5 className='text-2xl text-white font-semibold'>{user?.name}</h5>
-                  <p className='text-xl text-gray-400'>last seen today at 2:46 am </p>
+                  <p className='text-xl text-gray-400'>Last seen {moment().subtract('days').calendar()} </p>
                 </div>
               </div>
               <div>
@@ -79,25 +78,32 @@ const Message = () => {
                 </ul>
               </div>
             </div>
-            <div className=' w-screen h-screen bg-[url(./assets/image/bg-img-3.jpg)] fixed bg-no-repeat bg-cover bg-center bg-fixed text-white py-25 overflow-y-scroll'>
+            <div className='px-5 w-287 h-190 bg-[url(./assets/image/bg-img-3.jpg)] bg-no-repeat bg-cover bg-center overflow-y-scroll'>
+              { }
               {smslist.map((smsitem) => (
-                <div className='bg-gray-400'>
-                  <li className="mx-5 flex justify-between p-2 mb-3 border-b-2 border-gray-500/80 cursor-pointer hover:rounded-t-xl">
-                    <div className='flex gap-4 items-center'>
-                      {auth.currentUser.uid == smsitem.senderid ||
-                        auth.currentUser.uid == smsitem.reciverid &&
-                        <img className='w-10 h-10 my-1 rounded-full border-3 border-white/50 shadow-sm shadow-black/70'
-                          src={smsitem.senderphoto} alt="" />
-                      }
-                      <div className='w-full'>
-                        <p className={`text-md px-3 py-2 ${user?.id == smsitem.senderid ? "bg-orange-800 rounded-tr-2xl" : "bg-green-500 rounded-tl-2xl"} `}>{smsitem.sms}</p>
+                <div>
+                  {user?.id == smsitem.senderid ||
+                    user?.id == smsitem.reciverid ? (
+                    <div className="mb-3 cursor-pointer">
+                      <div className='flex'>
+                        <p className='text-md text-white my-1 m-auto'>{moment(smsitem.date, "YYYYMMDD,h:mm:ss a").fromNow()}</p>
+                      </div>
+                      <div className={`flex gap-4 items-end ${user?.id == smsitem.senderid ? "" : "justify-end"}`}>
+                        {auth.currentUser.uid == smsitem.reciverid &&
+                          <img className="w-10 h-10 my-1 rounded-full border-3 border-white/50 shadow-sm shadow-black/70"
+                            src={smsitem.senderphoto} alt="" />
+                        }
+                        <div className={` ${user?.id == smsitem.senderid ? "bg-gray-700/90 rounded-t-2xl rounded-br-2xl" : "bg-sky-900 rounded-t-2xl rounded-bl-2xl"}`}>
+                          <p className="relative text-lg text-white pt-2 px-3 mr-12 mb-4 text-justify">{smsitem.sms}
+                            <span className='absolute text-sm text-white -bottom-4 -right-12 px-3'>{moment(smsitem.date, "YYYYMMDD,h:mm:ss a").format("LT")}</span>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                    <p className='text-md text-white my-3'>Date or Time</p>
-                  </li>
+                  ) : ("")
+                  }
                 </div>
               ))}
-              <div className='bg-orange-500'></div>
             </div>
             <div className='flex  justify-between items-center absolute bottom-0 left-0 bg-sky-900/95 w-6xl h-25 py-5 px-10'>
               <button className='cursor-pointer'><FiPlus className='text-5xl text-gray-400 mr-6' /></button>
